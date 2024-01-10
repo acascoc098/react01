d5de8108559a4955556df81682dbcb4069c2baec
acascoc098 <acascoc098@g.educaand.es>
2024-01-10 14:20:39 +0100

    Segundo apartado.

--- src/index.js
+++ src/index.js
@@ -1,5 +1,16 @@
-import React from 'react';
+/*import React from 'react';
 import ReactDOM from 'react-dom/client';
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<h1>Hola perros</h1>);
+root.render(<h1>Hola perros</h1>);*/
+
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+const Titulo = ({subTitle})=> {
+return <div>
+ <h1>Esto es un título personalizado</h1>
+ <h2>{subTitle}</h2>
+</div>;
+}
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(<Titulo subTitle="Subtitulo"/>);
